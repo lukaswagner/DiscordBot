ac4c83190c94a7bb664b5de89eba61037f8f5d77
Lukas Wagner <lukas.wagner@student.hpi.uni-potsdam.de>
2017-04-26 01:12:53 +0200

    added support for commands, added placeholders for ban list module

---
+++ Src/Modules/BanList.cs
@@ -0,0 +1,71 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class BanListModule : ModuleBase
+    {
+        [Command("ban"), Summary("Ban a user.")]
+        public async Task Ban([Summary("The user to ban.")] IUser user, [Remainder, Summary("The reason for the ban.")] string reason)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("multiban"), Summary("Ban a user multiple times.")]
+        [Alias("mban")]
+        public async Task MultiBan([Summary("The user to ban.")] IUser user, [Summary("How often to ban the user.")] int amount, [Remainder, Summary("The reason for the bans.")] string reason)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("unban"), Summary("Unban a user.")]
+        public async Task Unban([Summary("The user to unban.")] IUser user, [Remainder, Summary("The reason for the unban.")] string reason)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("multiunban"), Summary("Unban a user multiple times.")]
+        [Alias("muban")]
+        public async Task MultiUnban([Summary("The user to ban.")] IUser user, [Summary("How often to ban the user.")] int amount, [Remainder, Summary("The reason for the bans.")] string reason)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("bancount"), Summary("Display how many times a user has been banned.")]
+        [Alias("bc")]
+        public async Task BanCount([Summary("The (optional) user to get info for")] IUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("scoreboard"), Summary("Display how many times each user has been banned.")]
+        public async Task Scoreboard()
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("banlist"), Summary("Link a list of all bans.")]
+        public async Task Banlist()
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+
+        [Command("recalculateBans"), Summary("Recalculate the number of bans for each user.")]
+        [Alias("rb")]
+        public async Task RecalculateBans()
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync("not implemented");
+        }
+    }
+}
--- Src/Program.cs
+++ Src/Program.cs
@@ -1,28 +1,44 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DiscordBot
 {
     public class Program
     {
-        public static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
+        private CommandService commands;
+        private DiscordSocketClient client;
+        private DependencyMap map;
+
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-            var client = new DiscordSocketClient();
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            map = new DependencyMap();
 
             client.Log += Log;
-            client.MessageReceived += MessageReceived;
 
-            string token = "MzA2NDY4OTYyODU2NTk5NTUz.C-ENDA.WDO_vWHrtiRsiZ4sVGq8FuqHGOs"; // Remember to keep this private!
+            string token = "MzA2NDY4OTYyODU2NTk5NTUz.C-ENDA.WDO_vWHrtiRsiZ4sVGq8FuqHGOs";
+
+            await InstallCommands();
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+        public async Task InstallCommands()
+        {
+            // Hook the MessageReceived Event into our Command Handler
+            client.MessageReceived += HandleCommand;
+            // Discover all of the commands in this assembly and load them.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
         }
 
         private Task Log(LogMessage msg)
@@ -31,12 +47,22 @@ namespace DiscordBot
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceived(SocketMessage message)
+        public async Task HandleCommand(SocketMessage messageParam)
         {
-            if (message.Content == "!ping")
-            {
-                await message.Channel.SendMessageAsync("Pong!");
-            }
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+            // Create a Command Context
+            var context = new CommandContext(client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed succesfully)
+            var result = await commands.ExecuteAsync(context, argPos, map);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
         }
     }
 }
