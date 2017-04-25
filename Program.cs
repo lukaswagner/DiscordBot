4cb4a69a8185783ea74909a38ca0c9e0f6d65893
Lukas Wagner <lukas.wagner@student.hpi.uni-potsdam.de>
2017-04-25 22:48:47 +0200

    switched to csproj project file, added sample discord code

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace MyBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+
+            string token = "MzA2NDY4OTYyODU2NTk5NTUz.C-ENDA.WDO_vWHrtiRsiZ4sVGq8FuqHGOs"; // Remember to keep this private!
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
