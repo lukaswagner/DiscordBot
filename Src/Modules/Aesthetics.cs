40b34c814e2fbb1fe1cbf91852182c01d832b18b
Lukas Wagner <lukas.wagner@student.hpi.uni-potsdam.de>
2017-05-24 01:17:07 +0200

    added fullwidth test converter (closes #9)

---
+++ Src/Modules/Aesthetics.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class Aesthetics : ModuleBase
+    {
+        [Command("aesthetic"), Summary("Aesthetify a message.")]
+        [Alias("aest", "jcostyle")]
+        public async Task Ban([Remainder, Summary("The string to make aesthetic.")] string message)
+        {
+            string result = "";
+            for(int index = 0; index < message.Length; index++)
+            {
+                char c = message[index];
+                byte[] b = BitConverter.GetBytes(c);
+                if (b[0] > 32 && b[0] < 127 && b[1] == 0)
+                {
+                    b[1] = 255;
+                    b[0] -= 32;
+                    result += BitConverter.ToChar(b, 0);
+                }
+                else if(b[0] == 32 && b[1] == 0)
+                {
+                    b[1] = 48;
+                    b[0] = 0;
+                    result += BitConverter.ToChar(b, 0);
+                }
+                else result += c;
+            }
+            await ReplyAsync(result);
+        }
+    }
+}
